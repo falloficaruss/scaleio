29e0161dfb3d76cfb61b8e46607a4ac5a471159e
Abhishek <norizzabhii@gmail.com>
2026-03-24 03:52:32 +0000

    some changes

--- inference.py
+++ inference.py
@@ -1,21 +1,22 @@
-import torch
 import cv2
 import numpy as np
+import torch
 from basicsr.arch.rrdbnet_arch import RRDBNet
-
 from realesrgan import RealESRGANer
 
-model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=4)
+model = RRDBNet(
+    num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=4
+)
 
-model_path = '4x-UltraSharp.pth'
+model_path = "4x-UltraSharp.pth"
 upscaler = RealESRGANer(
     scale=4,
     model_path=model_path,
     model=model,
     title=400,
-    device='cuda' if torch.cuda.is_available() else 'cpu'
+    device="cuda" if torch.cuda.is_available() else "cpu",
 )
 
-img = cv2.imread('input.jpg')
-ouptut, _ = upscaler.enhance(img, outscale=4)
-cv2.imwrite('output.png', output)
+img = cv2.imread("input.jpg")
+output, _ = upscaler.enhance(img, outscale=4)
+cv2.imwrite("output.png", output)
